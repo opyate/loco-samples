35d1a128a8703744ab6159fe2c70091a26461d1b
Tim Whitlock <r-github@timwhitlock.info>
2012-05-21 15:57:30 +0100

    Added simple YAML example for PHP

---
+++ examples/php/hello-world/yaml.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Example of using a simple YAML file to display translated content
+ */
+
+ // You will need the YAML extension installed
+extension_loaded('yaml') or die('yaml extension is not installed');
+ 
+ 
+// Grab current locale from utility function
+require 'utils/inc-example-functions.php';
+$locale = example_current_locale();
+
+// Language packs are in the data directory, under 'yaml/simple'
+$localepath = '../../../data/yaml/simple/locales/'.$locale.'/samples-'.$locale.'.yml';
+$localepath = realpath($localepath) and file_exists($localepath) or die('Cannot find the YAML locale file');
+
+// Translations stored in a single array in a return statement
+$messages = yaml_parse_file( $localepath );
+
+// Translate keys with A simple array lookup
+$key = 'hello_world';
+$message = $messages[$key] or $message = $key;
+
+
+?><!DOCTYPE html>
+<html lang="<?php echo $locale?>">
+<head>
+<meta charset="utf-8" />
+<title>Loco php examples - YAML - (<?php echo $locale?>)</title>
+</head>
+<body>
+    <h1><?php echo htmlentities( $message, ENT_COMPAT, 'UTF-8')?></h1>
+    <?php example_print_locale_menu()?> 
+</body>
+</html>
