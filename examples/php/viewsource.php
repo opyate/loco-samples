156d490b4157efe0ac68af690eff979ba79d6e18
Tim Whitlock <r-github@timwhitlock.info>
2012-05-26 12:33:16 +0100

    source view utf8

--- examples/php/viewsource.php
+++ examples/php/viewsource.php
@@ -2,5 +2,21 @@
 /**
  * Source hilighter for the PHP examples under this directory
  */
+header('Content-Type: text/html; charset=utf8'); 
 $path = realpath( './'.str_replace('..','.',$_GET['p']) ) or die('Path not found or not allowed');
-highlight_file($path);
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title>Loco PHP source | <?php echo basename($path)?></title>
+<style>
+code {
+    font: 9pt/9pt Monaco,'Courier New',Courier,monospace;
+}
+</style>
+</head>
+<body>
+<?php highlight_file($path); ?>
+</body>
+</html>
